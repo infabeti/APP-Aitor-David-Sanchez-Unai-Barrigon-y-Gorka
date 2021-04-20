68cc25875872697bbaf7619c003429817ff60d98
Aitor Calderon <aitor.calderonlo@elorrieta-errekamari.com>
2021-04-20 13:24:29 +0200

    Se crea testPanelesControladores, no se seguir ya que es una clase abstracta, mirar el siguiente dia

---
+++ MVCFinal/src/test/java/TestControlador/TestControladoresPaneles.java
@@ -0,0 +1,16 @@
+package TestControlador;
+
+import Controlador.Controlador;
+import Controlador.ControladoresPaneles;
+import Modelo.Modelo;
+import Vista.Vista;
+
+public class TestControladoresPaneles {
+
+	private Modelo modelo;
+	private Vista vista;
+	private Controlador controlador;
+
+	//private ControladoresPaneles controladoresPaneles = new ControladoresPaneles(modelo, vista, controlador);
+
+}
